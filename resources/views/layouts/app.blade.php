d8128701db5f356b0fcdaee2a90f39c52841eebb
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-02 14:50:46 +0200

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>My Wardrobe @yield('title')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,7 +18,6 @@
 <body>
 
     <main>
-
         @yield('content')
     </main>
 </body>
