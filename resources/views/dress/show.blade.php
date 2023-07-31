4f3e44a742894cb79e0d67705f111b3006fbc6df
giuseppevignanello <peppe.vignanello@gmail.com>
2023-07-31 09:57:56 +0200

    add edit button on show view

--- resources/views/dress/show.blade.php
+++ resources/views/dress/show.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="mt-2 ms-2">
-        <a class="btn btn-sm bg-light" href="{{ route('dress.index') }}" role="button"><i
-                class="fa-solid fa-arrow-left"></i></a>
+    <div class="d-flex mt-2 ms-2">
+        <div>
+            <a class="btn btn-sm bg-light" href="{{ route('dress.index') }}" role="button"><i
+                    class="fa-solid fa-arrow-left"></i></a>
+            <a class="btn btn-sm bg_secondary text-white" href="{{ $dress->id }}/edit"> <i
+                    class="fa-solid fa-pen-to-square"></i> </a>
+        </div>
+        <div>
+
+        </div>
     </div>
     <div class="show_card card bg_opacity text-white">
         <img class="card-img-top show_img" src="{{ asset('storage/' . $dress->image) }}" alt="Title">
