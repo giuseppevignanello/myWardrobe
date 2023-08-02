0bcf2bad9b5f5da9b2d23a4972907b0ee84467d8
giuseppevignanello <peppe.vignanello@gmail.com>
2023-08-02 10:50:18 +0200

    add some style

--- resources/views/outifit/index.blade.php
+++ resources/views/outifit/index.blade.php
@@ -14,18 +14,16 @@
             <ul class="list-unstyled">
                 @foreach ($outfits as $outfit)
                     <li>
-                        <div class="row justify-content-between bg-white">
-                            <div class="col-5">
-                                <h4 class="card-title">{{ $outfit->name }}</h4>
+                        <div
+                            class="outfit_index_card row justify-content-between bg_secondary text-white align-items-center mb-2 rounded-1">
+                            <div class="col-6 p-3">
+                                <h5 class="card-title">{{ $outfit->name }}</h5>
                                 <p class="card-text">{{ $outfit->occasion }} <br>
                                     {{ $outfit->season }}
                                 </p>
                             </div>
-                            <div class="d-flex gap-2 col-4">
-                                @foreach ($outfit->dresses as $dress)
-                                    <img style="height: 100px; width:100px" src={{ asset('storage/' . $dress->image) }}
-                                        alt="">
-                                @endforeach
+                            <div class="d-flex gap-2 col-5 justify-content-end">
+                                <img class="index_outfit_image" src="https://picsum.photos/100/100" alt="">
                             </div>
                         </div>
                     </li>
