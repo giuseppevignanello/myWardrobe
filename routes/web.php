77dd9ce01cb78c25cc76d72614c8aab2f67184d7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-11-25 19:01:09 +0100

    fix outfit create bug

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,6 @@ Route::middleware('auth')->group(function () {
     Route::resource('dress', DressController::class);
     Route::resource('brand', BrandController::class);
     Route::resource('outfit', OutfitController::class);
-    Route::post('/outfit', [OutfitController::class, 'store'])->name('outfit.storeP');
-    Route::post('/outfit', [OutfitController::class, 'update'])->name('outfit.updateP');
 });
 
 require __DIR__ . '/auth.php';
