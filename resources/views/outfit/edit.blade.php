bef6189d625625159e6c1bf45dde98a5895d7110
giuseppevignanello <peppe.vignanello@gmail.com>
2023-12-14 15:25:01 +0100

    remove wardrobe from outfit section

--- resources/views/outfit/edit.blade.php
+++ resources/views/outfit/edit.blade.php
@@ -9,37 +9,6 @@
                     class="fa-solid fa-arrow-left"></i></a>
         </div>
 
-
-        <div class="wardrobe">
-            <div class="front">
-                <div class="door"></div>
-                <div class="second-door"></div>
-            </div>
-            <ul class="list-unstyled d-flex justify-content-center">
-                @forelse ($dresses as $dress)
-                    <li id={{ $dress->id }} class="outfit_dress d-none">
-                        <div class="card outfit_create_card">
-                            <img class="" src="{{ asset('storage/' . $dress->image) }}" alt={{ $dress->name }}>
-                            <div class="card-body text-white">
-                                <h6 class="card-title">{{ $dress->name }}</h6>
-                                <div class="d-flex gap-2 align-items-center">
-                                    <div class="card-text">{{ $dress->brand }}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                @empty
-                @endforelse
-            </ul>
-            <div class="shadow"></div>
-            <div class="buttons position-absolute">
-                <button id="prev" type="button" class="btn btn-sm bg_secondary text-white"><i
-                        class="fa-solid fa-arrow-left"></i></button>
-                <button id="next" class="btn btn-sm bg_secondary text-white"><i
-                        class="fa-solid fa-arrow-right"></i></button>
-            </div>
-        </div>
-
     </div>
     <div class="container">
         <ul id="outfit" class="d-flex list-unstyled mt-5 justify-content-center">
